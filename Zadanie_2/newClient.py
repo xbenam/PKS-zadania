ba535614969e8033ffd51e9115e4c9934e53ba5d
xbenam <xbenam@stuba.sk>
2022-12-08 21:56:32 +0100

    update

--- Zadanie_2/newClient.py
+++ Zadanie_2/newClient.py
@@ -5,16 +5,16 @@ def client_program():
     host = socket.gethostname()  # as both code is running on same pc
     port = 5000  # socket server port number
 
-    client_socket = socket.socket()  # instantiate
-    client_socket.connect(('LAPTOP-UQ1G9V9A', port))  # connect to the server
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # instantiate
+    client_socket.connect(("169.254.252.122", port))  # connect to the server
 
     message = input(" -> ")  # take input
 
     while message.lower().strip() != 'bye':
         client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
+        # data = client_socket.recv(1024).decode()  # receive response
 
-        print('Received from server: ' + data)  # show in terminal
+        # print('Received from server: ' + data)  # show in terminal
 
         message = input(" -> ")  # again take input
 
--- Zadanie_2/newServer.py
+++ Zadanie_2/newServer.py
@@ -7,21 +7,22 @@ def server_program():
     print(host)
     port = 5000  # initiate port no above 1024
 
-    server_socket = socket.socket()  # get instance
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # get instance
     # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
+    server_socket.bind(("169.254.252.122", port))  # bind host address and port together
 
     # configure how many client the server can listen simultaneously
-    server_socket.listen(5)
-    conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))
+    # server_socket.listen(1)
+    # conn, address = server_socket.accept()  # accept new connection
+    # print("Connection from: " + str(address))
     while True:
         # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
+        # data = conn.recv(1024).decode()
+        data, addr = server_socket.recvfrom(1024)
         if not data:
             # if data is not received break
             break
-        print("from connected user: " + str(data))
+        print("from "+ addr[0] + ": " + str(data.decode()))
         # data = input(' -> ')
         # conn.send(data.encode())  # send data to the client
 
