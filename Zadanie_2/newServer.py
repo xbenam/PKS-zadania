00d711bd9f41a58c7d7abcd872d099723cdaff52
xbenam <xbenam@stuba.sk>
2022-12-12 19:48:26 +0100

    Removing Server file

--- Zadanie_2/newServer.py
+++
@@ -1,8 +0,0 @@
-import communication_node
-
-if __name__ == '__main__':
-    ser = newClient.communication_node()
-    # ser.create_socket()
-    ser.run_server(input("IP: "), int(input("Port:")))
-    # ser.server_part()
-
